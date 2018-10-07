5402d4fdeb2ad269fe43025c9261b425ee1c17a6
Paula Caicedo <paulacaicedo03@hotmail.com>
2018-10-07 18:15:21 -0400

    final push

--- bamazon.js
+++ bamazon.js
@@ -1,111 +1,66 @@
-//require mysql and inquirer
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-//create connection to db
+var mysql = require("mysql");
+var cTable = require('console.table');
+var inquirer = require("inquirer");
+
 var connection = mysql.createConnection({
   host: "localhost",
+
+  // Your port; if not 3306
   port: 8889,
+
+  // Your username
   user: "root",
+
+  // Your password
   password: "root",
-  database: "Bamazon"
-})
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  
+});
+
+function connectionEnd(){
+    connection.end();
+}
 
 function start(){
-//prints the items for sale and their details
-connection.query('SELECT * FROM Products', function(err, res){
-  if(err) throw err;
+    connection.query("SELECT item_id, product_name, price FROM products", function (err,result){
+        if (err) throw err;     
 
-  console.log('_.~"~._.~"~._.~Welcome to BAMazon~._.~"~._.~"~._')
-  console.log('----------------------------------------------------------------------------------------------------')
+        const table = cTable.getTable(result);
+        console.log(table);
+        inquirer.prompt([
+            { type: "input",
+                name: "productid",
+                message: "What product would you like to buy?"
+        },{ type: "input",
+        name: "productquantity",
+        message: "How many would you like to buy?"
+}]).then(function(answers){
+    connection.query("SELECT stock_quantity, price FROM products WHERE item_id = ?", parseInt(answers.productid) ,function (err,res){
+        if (err) throw err; 
+        //console.log(res[0].stock_quantity + "----"+ parseInt(answers.productquantity))
+        if(res[0].stock_quantity >= parseInt(answers.productquantity)){
+            var newStock = res[0].stock_quantity - parseInt(answers.productquantity)
+            connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newStock,parseInt(answers.productid)],function (err,r){
+                console.log("Your total is: $" + (res[0].price*answers.productquantity));
 
-  for(var i = 0; i<res.length;i++){
-    console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
-    console.log('--------------------------------------------------------------------------------------------------')
-  }
+            });
+            connectionEnd();
 
-  console.log(' ');
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "id",
-      message: "What is the ID of the product you would like to purchase?",
-      validate: function(value){
-        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
-          return true;
-        } else{
-          return false;
+        }else{
+            console.log("Insufficient quantity!");
+            connectionEnd();
         }
-      }
-    },
-    {
-      type: "input",
-      name: "qty",
-      message: "How much would you like to purchase?",
-      validate: function(value){
-        if(isNaN(value)){
-          return false;
-        } else{
-          return true;
-        }
-      }
-    }
-    ]).then(function(ans){
-      var whatToBuy = (ans.id)-1;
-      var howMuchToBuy = parseInt(ans.qty);
-      var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
 
-      //check if quantity is sufficient
-      if(res[whatToBuy].StockQuantity >= howMuchToBuy){
-        //after purchase, updates quantity in Products
-        connection.query("UPDATE Products SET ? WHERE ?", [
-        {StockQuantity: (res[whatToBuy].StockQuantity - howMuchToBuy)},
-        {ItemID: ans.id}
-        ], function(err, result){
-            if(err) throw err;
-            console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
-        });
-
-        connection.query("SELECT * FROM Departments", function(err, deptRes){
-          if(err) throw err;
-          var index;
-          for(var i = 0; i < deptRes.length; i++){
-            if(deptRes[i].DepartmentName === res[whatToBuy].DepartmentName){
-              index = i;
-            }
-          }
-          
-          //updates totalSales in departments table
-          connection.query("UPDATE Departments SET ? WHERE ?", [
-          {TotalSales: deptRes[index].TotalSales + grandTotal},
-          {DepartmentName: res[whatToBuy].DepartmentName}
-          ], function(err, deptRes){
-              if(err) throw err;
-              //console.log("Updated Dept Sales.");
-          });
-        });
-
-      } else{
-        console.log("Sorry, there's not enough in stock!");
-      }
-
-      reprompt();
     })
-})
-}
+        })
+    });
 
-//asks if they would like to purchase another item
-function reprompt(){
-  inquirer.prompt([{
-    type: "confirm",
-    name: "reply",
-    message: "Would you like to purchase another item?"
-  }]).then(function(ans){
-    if(ans.reply){
-      start();
-    } else{
-      console.log("See you soon!");
-    }
-  });
-}
+    
+ };
 
-start();
+ start();
--- bamazon.sql
+++ bamazon.sql
@@ -1,37 +1,32 @@
-CREATE DATABASE bamazon_db;
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
 
-USE bamazon_db;
+USE bamazon;
 
+CREATE TABLE products (
+    item_id INT NOT NULL auto_increment,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INT NULL,
 
-CREATE TABLE products(	
-	id INT AUTO_INCREMENT NOT NULL,
-	description VARCHAR(30) NOT NULL,
-	department VARCHAR(30),
-	price FLOAT(11,2),
-	quantity INT(11),
-	PRIMARY KEY (id)
+    PRIMARY KEY (item_id)
 );
 
+USE bamazon;
 
-CREATE TABLE departments(
-	department_id INT(11) NOT NULL,
-	department_name VARCHAR(30) NOT NULL,
-	over_head_costs INT(11) NOT NULL,
-	PRIMARY KEY (department_id)
-);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 
-
-INSERT INTO products (description, department, price, quantity)
-VALUE 
-('Trek Marlin 4', 'Bicycles', 399.99, 3), 
-('Trek Skye WSD', 'Bicycles', 399.99, 2),
-('Trek Roscoe 8', 'Bicycles', 1199.99, 5),
-('Trek Domane 5.2', 'Bicycles', 3399.99, 1),
-('Trek Farley 9.6', 'Bicycles', 2699.99, 6),
-('Trek Fuel 8', 'Bicycles', 2999.99, 3),
-('Trek Top Fuel', 'Bicycles', 5199.99, 4),
-('Trek Lexa', 'Bicycles', 799.99, 2);
-
+VALUES 
+("Bire TV", "Electronics", 79.99, 50), 
+("Bindle", "Electronics", 49.99, 50), 
+("Becho", "Electronics", 179.99, 100), 
+("Xbox One", "Electronics", 199.99, 50), 
+("Rich Dad, Poor Dad", "Books", 11.99, 200), 
+("Hanes T-shirt", "Clothing", 4.99, 75), 
+("Sterling Silver Earings", "Jewelry", 29.50, 25), 
+("Crayon Pack", "Toys and Games", .99, 100), 
+("Camera", "Camera and Photo", 129.99, 50);
 
 
 
